44a196eff3c52a1e6819a24e52ef3899d1fff29b
TeeJay <627894583@qq.com>
2017-09-21 11:04:17 +0800

    javascript_ajax
    Simple way

---
+++ ajax.js
@@ -0,0 +1,30 @@
+var ajax = {
+    get: function (url, successfn) {
+        var xml = new XMLHttpRequest();
+        xml.open('GET', url, true);
+        xml.onreadystatechange = function () {
+            if (xml.readyState === 4 && xml.status === 200 || xml.status === 304) {
+                successfn.call(this, xml.responseText);
+            }
+        };
+        xml.send();
+    },
+    post: function (url, headers, data, successfn) {
+        var xml = new XMLHttpRequest();
+        xml.open('POST', url, true);
+        if (headers) {
+            for (var x in headers) {
+                xml.setRequestHeader(x, headers[x]);
+            }
+        } else {
+            xml.setRequestHeader("Content-type", "Application/json;charset=utf-8");
+        }
+
+        xml.onreadystatechange = function () {
+            if (xml.readyState === 4 && xml.status === 200 || xml.status === 304) {
+                successfn.call(this, xml.responseText);
+            }
+        }
+        xml.send(JSON.stringify(data));
+    }
+}
